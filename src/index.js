329399a6b65753db925db3b001c416f18660a0ac
binhosemcrause <binhosemcrause@gmail.com>
2024-09-23 21:17:32 +1000

    upload project

---
+++ src/index.js
@@ -0,0 +1,16 @@
+//Create a react app from scratch.
+//It should display 2 paragraph HTML elements.
+//The paragraphs should say:
+//Created by YOURNAME.
+//Copyright CURRENTYEAR.
+//E.g.
+//Created by Robson Silva.
+//Copyright 2024.
+
+// If you're running this locally in VS Code use the commands:
+// npm install
+// to install the node modules and
+// npm start
+// to launch your react project in your browser
+
+
